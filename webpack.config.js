27e638f7f1faf6299dbea4bcfbbd4e7b675ec2ba
Sebras22 <sebastien.branly@efrei.net>
2026-03-03 11:03:30 +0100

    Checkpoint Shell

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,55 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+        publicPath: "http://localhost:3000/",
+        clean: true,
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "shell",
+            remotes: {},
+            shared: {
+                react: { singleton: true, requiredVersion: "^18.2.0" },
+                "react-dom": { singleton: true, requiredVersion: "^18.2.0" },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
