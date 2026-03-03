9569917b98a42dc2237046b9568962f582f39c57
Sebras22 <sebastien.branly@efrei.net>
2026-03-03 11:05:39 +0100

    feat: add initial HTML structure and main App component for PixelArena

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>PixelArena Shell</h1>
+            <p>Application principale prête à charger les micro-frontends.</p>
+        </div>
+    );
+};
+
+export default App;
